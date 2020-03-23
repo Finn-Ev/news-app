110133adbb47eb9845b5ae38363c3aa85227a9e0
Finn Evermann <52169228+Finn-Ev@users.noreply.github.com>
2020-03-23 14:38:53 +0100

    Add files via upload

---
+++ dist/precache-manifest.9bf35e3eeaa60275eaaf131d0b28ab5f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f912144fca0e4654305",
+    "url": "/css/app.8104caa1.css"
+  },
+  {
+    "revision": "5fcc994868ea3875b1ce",
+    "url": "/css/chunk-01e985d6.c73d54ab.css"
+  },
+  {
+    "revision": "e9f9e1f043a47cd2820604cde862b5e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f912144fca0e4654305",
+    "url": "/js/app.c9753472.js"
+  },
+  {
+    "revision": "5fcc994868ea3875b1ce",
+    "url": "/js/chunk-01e985d6.95460473.js"
+  },
+  {
+    "revision": "b71634f92261a954971c",
+    "url": "/js/chunk-2d0c1d78.5f920d8d.js"
+  },
+  {
+    "revision": "e7825d94a0c31ffb8a35",
+    "url": "/js/chunk-vendors.09cfa198.js"
+  },
+  {
+    "revision": "cb8c6c24c0d2ef3af0e22d83a861bce0",
+    "url": "/manifest.json"
+  }
+]);
