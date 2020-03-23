3029cd5f002573bb73852bb3b04608f80e7b4fbd
Finn Evermann <finnevermann@gmx.de>
2020-03-23 15:21:53 +0100

    Updates

---
+++ precache-manifest.47ec788c338bb4006d8466d3deb9b605.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0d31f256de6a595bda3",
+    "url": "/css/app.8104caa1.css"
+  },
+  {
+    "revision": "8db69d1f3db4baaa29ff",
+    "url": "/css/chunk-01e985d6.c73d54ab.css"
+  },
+  {
+    "revision": "8374c2f4f3db1cb8830e8240176fff72",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0d31f256de6a595bda3",
+    "url": "/js/app.58235099.js"
+  },
+  {
+    "revision": "8db69d1f3db4baaa29ff",
+    "url": "/js/chunk-01e985d6.54f780cc.js"
+  },
+  {
+    "revision": "0739fddb7ec06f699e7f",
+    "url": "/js/chunk-2d0c1d78.1dcae50b.js"
+  },
+  {
+    "revision": "e7825d94a0c31ffb8a35",
+    "url": "/js/chunk-vendors.09cfa198.js"
+  },
+  {
+    "revision": "cb8c6c24c0d2ef3af0e22d83a861bce0",
+    "url": "/manifest.json"
+  }
+]);
